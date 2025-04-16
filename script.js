2727b1d3249312ee5ec2919aead084a2fc7b6eef
Varun Kumar <varunkumar1329@gmail.com>
2025-04-16 20:17:47 +0530

    commit it

--- script.js
+++ script.js
@@ -163,3 +163,22 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
+//Prevent righ click
+document.oncontextmenu = () => {
+    alert("Don't try right click")
+    return false
+}
+
+document.onkeydown = e => {
+    //Prevent F12 key
+    if(e.key == "F12"){
+        alert("Don't try to inspect element")
+        return false
+    }
+
+    //Prevent showing page source by ctrl + U
+    if(e.ctrlKey && e.key == "u") {
+        alert("Don't try to view page sources")
+        return false
+    }
+}
